311d5bd4ed81ecc36a0e0c43cb85637056dd5a6a
trzhang-ai <32316075+trzhang-ai@users.noreply.github.com>
2026-06-26 02:44:29 +0800

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,11 @@
+def add_two_numbers() -> int:
+    inputs = input()
+    return sum([int(i) for i in inputs.split(',')])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
